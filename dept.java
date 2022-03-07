ad48d2386d25e529095461512a80182deec61bac
Realkop99 <lfcparijat@gmail.com>
2022-03-07 11:07:45 +0530

    first add in localrepo

---
+++ dept.java
@@ -0,0 +1,13 @@
+public class Dept
+{
+	private int deptno;
+	private string dname;
+	private String location;
+	public Dept(){}
+	public Dept(int deptno,String dname, String location)
+	{
+		this.deptno=deptno;
+		this.dname=dname;
+		this.location=location;
+	}
+}
